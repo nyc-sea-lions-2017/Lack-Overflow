0edf83ef725f8a12f32d9c8ee5ea084901e242c5
Teddy Koomen <email": null>
2017-06-09 15:48:28 -0400

    add route protection for comments

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,17 +1,25 @@
-get 'questions/:question_id/comments/new' do
-  erb :comment
+get '/questions/:question_id/comments/new' do
+  if current_user
+    erb :comment
+  else
+    redirect '/login'
+  end
 end
 
-post 'questions/:question_id/comments' do
+post '/questions/:question_id/comments' do
   @comment = Comment.create(text: params[:text], commenter_id: session[:id], commentable_id: params[:question_id], commentable_type: "question")
-  erb :'/questions/<%= params[:question_id] %>'
+  redirect "/questions/#{params[:question_id]}"
 end
 
-get 'answers/:answer_id/comments/new' do
-  erb :comment
+get '/answers/:answer_id/comments/new' do
+  if current_user
+    erb :comment
+  else
+    redirect '/'
+  end
 end
 
-post 'answers/:answer_id/comments' do
+post '/answers/:answer_id/comments' do
   @comment = Comment.create(text: params[:text], commenter_id: session[:id], commentable_id: params[:answer_id], commentable_type: "answer")
   erb :'/answers/<%= params[:answer_id] %>'
 end
