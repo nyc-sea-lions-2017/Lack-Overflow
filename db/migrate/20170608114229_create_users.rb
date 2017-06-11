a8820191bb4c90880b2dd0ef310ce0102b1a2005
cfrancisco726 <cfrancisco726@gmail.com>
2017-06-10 22:41:22 -0400

    add uniqueness true to user migration

--- db/migrate/20170608114229_create_users.rb
+++ db/migrate/20170608114229_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
+      t.string :name, null: false 
+      t.string :email, null: false, uniqueness: true
       t.string :password_digest, null: false
 
       t.timestamps
